d0d6a3aa2c4a6e77315f0ac7891ed0fdbb9daaf6
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-08 07:09:27 -0800

    Commit

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -3,12 +3,17 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 
 const Appbar = () => {
-    const [title, settitle] = useState('This is a React Component')
+    const [count, settitle] = useState(10)
 
+    let classes = "mt-4 badge m-4 p-4";
+    classes += (count == 0) ? "badge-warning" : "badge-primary";
+
+
+    
     return (
         <div>
             <center>
-                <span style={{fontSize: 30}} className="mt-4 badge badge-warning m-4 p-4">{title}</span>
+                <span style={{fontSize: 30}} className={classes}>{count == 0 ? 'Zero' : count}</span>
                 <br/>
                 <button className="btn btn-primary btn-sm mt-4">Increment</button>
             </center>
